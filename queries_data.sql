55d57d1932cc5f263520996f5b25809e0a69ba4d
cesaraugusto316 <cesaraugusto316@protonmail.com>
2022-09-18 11:32:52 -0500

    average amount/price per aircraft added

--- queries_data.sql
+++ queries_data.sql
@@ -30,3 +30,15 @@ inner join flights on flights.arrival_airport = airports_data.airport_code or fl
 group by airports_data.airport_code
 order by count(airports_data.airport_code) asc
 limit 3;
+
+-- mostrar el valor promedio de los tickets por avión
+-- show the average ticket amount by aircraft
+
+-- every tikect_flight has three catgeories: Economy, Business, Comfort	
+-- every flight_ticket has amount and flight_id, and every flight_id has an aircraft_code
+
+select avg(ticket_flights.amount) as average, aircrafts_data.model from ticket_flights
+  inner join flights on flights.flight_id = ticket_flights.flight_id 
+  inner join aircrafts_data on flights.aircraft_code = aircrafts_data.aircraft_code
+  group by aircrafts_data.model
+  order by average desc;
